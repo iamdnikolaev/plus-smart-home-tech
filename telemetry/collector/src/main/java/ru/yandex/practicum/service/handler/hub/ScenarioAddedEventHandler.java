22e89908a449b73b61a484e1f26ae865a4b918ad
iamdnikolaev <160739477+iamdnikolaev@users.noreply.github.com>
2025-03-03 09:56:12 +0300

    Спринт 20. Collector - GRPC. Согласно примеру из теории: 1) добавлены поля в запись ScenarioAddedEventProto; 2) исправлена нумерация полей ClimateSensorProto.

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/handler/hub/ScenarioAddedEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/handler/hub/ScenarioAddedEventHandler.java
@@ -1,12 +1,20 @@
 package ru.yandex.practicum.service.handler.hub;
 
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.grpc.telemetry.event.DeviceActionProto;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.grpc.telemetry.event.ScenarioAddedEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.ScenarioConditionProto;
+import ru.yandex.practicum.kafka.telemetry.event.ConditionOperationAvro;
+import ru.yandex.practicum.kafka.telemetry.event.ConditionTypeAvro;
+import ru.yandex.practicum.kafka.telemetry.event.DeviceActionAvro;
+import ru.yandex.practicum.kafka.telemetry.event.DeviceActionTypeAvro;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.ScenarioAddedEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.ScenarioConditionAvro;
 
 import java.time.Instant;
+import java.util.List;
 
 @Component
 public class ScenarioAddedEventHandler extends HubProtoHandler {
@@ -17,6 +25,13 @@ public class ScenarioAddedEventHandler extends HubProtoHandler {
 
     public HubEventAvro mapToAvro(HubEventProto eventProto) {
         ScenarioAddedEventProto scenarioAddedEventProto = eventProto.getScenarioAdded();
+
+        List<ScenarioConditionAvro> scenarioConditionAvroList = scenarioAddedEventProto.getConditionList()
+                .stream().map(this::mapToScenarioConditionAvro).toList();
+
+        List<DeviceActionAvro> deviceActionAvroList = scenarioAddedEventProto.getActionList()
+                .stream().map(this::mapToDeviceActionAvro).toList();
+
         ScenarioAddedEventAvro scenarioAddedEventAvro = ScenarioAddedEventAvro.newBuilder()
                 .setName(scenarioAddedEventProto.getName())
                 .build();
@@ -26,6 +41,35 @@ public class ScenarioAddedEventHandler extends HubProtoHandler {
                 .setTimestamp(Instant.ofEpochSecond(eventProto.getTimestamp().getSeconds(),
                         eventProto.getTimestamp().getNanos()))
                 .setPayload(scenarioAddedEventAvro)
+                .build();
+    }
+
+    private ScenarioConditionAvro mapToScenarioConditionAvro(ScenarioConditionProto scenarioConditionProto) {
+        Object value;
+        switch (scenarioConditionProto.getValueCase()) {
+            case ScenarioConditionProto.ValueCase.BOOL_VALUE:
+                value = scenarioConditionProto.getBoolValue();
+                break;
+            case ScenarioConditionProto.ValueCase.INT_VALUE:
+                value = scenarioConditionProto.getIntValue();
+                break;
+            default:
+                value = null;
+        }
+
+        return ScenarioConditionAvro.newBuilder()
+                .setSensorId(scenarioConditionProto.getSensorId())
+                .setType(ConditionTypeAvro.valueOf(scenarioConditionProto.getType().name()))
+                .setOperation(ConditionOperationAvro.valueOf(scenarioConditionProto.getOperation().name()))
+                .setValue(value)
+                .build();
+    }
+
+    private DeviceActionAvro mapToDeviceActionAvro(DeviceActionProto deviceActionProto) {
+        return DeviceActionAvro.newBuilder()
+                .setSensorId(deviceActionProto.getSensorId())
+                .setType(DeviceActionTypeAvro.valueOf(deviceActionProto.getType().name()))
+                .setValue(deviceActionProto.getValue())
                 .build();
     }
 }
