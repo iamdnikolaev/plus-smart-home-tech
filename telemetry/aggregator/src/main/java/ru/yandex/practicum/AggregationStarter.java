c256eb63053b773a848d43ddd9c4565a5302f5a2
iamdnikolaev <160739477+iamdnikolaev@users.noreply.github.com>
2025-03-06 16:37:01 +0300

    Спринт 20. Aggregator. Переделано через бины с конфигурацией.

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregationStarter.java
@@ -2,58 +2,44 @@ package ru.yandex.practicum;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.avro.specific.SpecificRecordBase;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.errors.WakeupException;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorStateAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
-import ru.yandex.practicum.service.AvroSerializer;
-import ru.yandex.practicum.service.SensorEventDeserializer;
 
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Properties;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class AggregationStarter {
-    @Value("${kafka.bootstrap-servers}")
-    private final String BOOTSTRAP_SERVERS_CONFIG;
-    private final Duration CONSUME_ATTEMPT_TIMEOUT = Duration.ofMillis(1000);
+    private Duration consumeAttemptTimeout = Duration.ofMillis(1000);
     @Value("${topic.telemetry.sensors}")
-    private final String SENSORS_TOPIC;
+    private String topicTelemetrySensors;
     @Value("${topic.telemetry.snapshots}")
-    private final String SNAPSHOTS_TOPIC;
-    @Value("${aggregator-client-id}")
-    private final String AGGREGATOR_CLIENT_ID;
-    @Value("${aggregator-group-id}")
-    private final String AGGREGATOR_GROUP_ID;
+    private String topicTelemetrySnapshots;
 
-    private final KafkaConsumer<String, SensorEventAvro> consumer = new KafkaConsumer<>(getConsumerProperties());
-    private final KafkaProducer<String, SpecificRecordBase> producer = new KafkaProducer<>(getProducerProperties());
-    private final Map<String, SensorsSnapshotAvro> snapshots = new HashMap<>();
+    private final KafkaConsumer<String, SensorEventAvro> kafkaConsumer;
+    private final KafkaProducer<String, SensorsSnapshotAvro> kafkaProducer;
+    private Map<String, SensorsSnapshotAvro> snapshots = new HashMap<>();
 
     public void start() {
         try {
-            consumer.subscribe(List.of(SENSORS_TOPIC));
+            kafkaConsumer.subscribe(List.of(topicTelemetrySensors));
 
             while (true) {
-                ConsumerRecords<String, SensorEventAvro> records = consumer.poll(CONSUME_ATTEMPT_TIMEOUT);
+                ConsumerRecords<String, SensorEventAvro> records = kafkaConsumer.poll(consumeAttemptTimeout);
 
                 for (ConsumerRecord<String, SensorEventAvro> record : records) {
                     SensorEventAvro event = record.value();
@@ -61,17 +47,17 @@ public class AggregationStarter {
 
                     if (sensorsSnapshotAvro.isPresent()) {
                         SensorsSnapshotAvro snapshotAvro = sensorsSnapshotAvro.get();
-                        ProducerRecord<String, SpecificRecordBase> producerRecord =
-                                new ProducerRecord<>(SNAPSHOTS_TOPIC,
+                        ProducerRecord<String, SensorsSnapshotAvro> producerRecord =
+                                new ProducerRecord<>(topicTelemetrySnapshots,
                                         null,
                                         snapshotAvro.getTimestamp().toEpochMilli(),
                                         snapshotAvro.getHubId(),
                                         snapshotAvro);
-                        producer.send(producerRecord);
-                        log.info("snapshotAvro {} has sent on topic {}", snapshotAvro, SNAPSHOTS_TOPIC);
+                        kafkaProducer.send(producerRecord);
+                        log.info("snapshotAvro {} has sent on topic {}", snapshotAvro, topicTelemetrySnapshots);
                     }
                 }
-                consumer.commitAsync();
+                kafkaConsumer.commitAsync();
             }
 
         } catch (WakeupException ignored) {
@@ -80,33 +66,15 @@ public class AggregationStarter {
             log.warn("Sensor events have got an error", e);
         } finally {
             try {
-                producer.flush();
-                consumer.commitSync();
+                kafkaProducer.flush();
+                kafkaConsumer.commitSync();
             } finally {
                 log.info("Consumer is closing");
-                consumer.close();
+                kafkaConsumer.close();
                 log.info("Producer is closing");
-                producer.close();
+                kafkaProducer.close();
             }
         }
-    }
-
-    private Properties getConsumerProperties() {
-        Properties properties = new Properties();
-        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, AGGREGATOR_CLIENT_ID);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, AGGREGATOR_GROUP_ID);
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
-        return properties;
-    }
-
-    private Properties getProducerProperties() {
-        Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class);
-        return properties;
     }
 
     private Optional<SensorsSnapshotAvro> updateState(SensorEventAvro event) {
