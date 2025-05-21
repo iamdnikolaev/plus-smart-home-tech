f65c4832a4f9d3a8668b2a3f729cd711fbed31d9
iamdnikolaev <160739477+iamdnikolaev@users.noreply.github.com>
2025-05-21 13:41:22 +0300

    Спринт 22. Подключен gateway.

---
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/ErrorHandler.java
@@ -0,0 +1,62 @@
+package ru.yandex.practicum.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.exception.ErrorResponse;
+import ru.yandex.practicum.exception.ProductNotFoundException;
+
+import java.util.Arrays;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleCommonException(RuntimeException e) {
+        log.error("Handle with status INTERNAL_SERVER_ERROR for error: {}", e.getMessage());
+        return ErrorResponse.builder()
+                .cause(e.getCause())
+                .stackTrace(Arrays.asList(e.getStackTrace()))
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .userMessage(e.getMessage())
+                .message("!!!INTERNAL_SERVER_ERROR!!!")
+                .suppressed(Arrays.asList(e.getSuppressed()))
+                .localizedMessage(e.getLocalizedMessage())
+                .build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleProductNotFoundException(ProductNotFoundException e) {
+        log.error("Handle with status NOT_FOUND for error: {}", e.getMessage());
+        return ErrorResponse.builder()
+                .cause(e.getCause())
+                .stackTrace(Arrays.asList(e.getStackTrace()))
+                .httpStatus(HttpStatus.NOT_FOUND.name())
+                .userMessage(e.getMessage())
+                .message("!!!NOT_FOUND!!!")
+                .suppressed(Arrays.asList(e.getSuppressed()))
+                .localizedMessage(e.getLocalizedMessage())
+                .build();
+    }
+
+    @ExceptionHandler({MissingServletRequestParameterException.class, MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(RuntimeException e) {
+        log.error("Handle with status BAD_REQUEST for error: {}", e.getMessage());
+        return ErrorResponse.builder()
+                .cause(e.getCause())
+                .stackTrace(Arrays.asList(e.getStackTrace()))
+                .httpStatus(HttpStatus.BAD_REQUEST.name())
+                .userMessage(e.getMessage())
+                .message("!!!BAD_REQUEST!!!")
+                .suppressed(Arrays.asList(e.getSuppressed()))
+                .localizedMessage(e.getLocalizedMessage())
+                .build();
+    }
+}
